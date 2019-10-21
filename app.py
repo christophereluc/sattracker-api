3083357ecaecb49b47ea1a18ba1f825e39773e98
Kamille Delgardo <hecate@hecate.local>
2019-10-20 23:54:11 -0700

    connet cleardb mysql database

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask
-# from flask_mysqldb import MySQL
+from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
 
 # set up mysql database
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = 'root'
-# app.config['MYSQL_DB'] = 'MyDB'
-#
-# mysql = MySQL(app)
+app.config['MYSQL_HOST'] = 'us-cdbr-iron-east-05.cleardb.net'
+app.config['MYSQL_USER'] = os.environ["HAM_DATABASE_USER"]
+app.config['MYSQL_PASSWORD'] = os.environ["HAM_DATABASE_PASSWORD"]
+app.config['MYSQL_DB'] = 'heroku_95aba217f91d579'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
