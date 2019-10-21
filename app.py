f40ff5ca65b7dc63f982be8b9ba5b63c54ce8495
Kamille Delgardo <hecate@hecate.local>
2019-10-20 23:02:42 -0700

    start without database

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_mysqldb import MySQL
+# from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
 # set up mysql database
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'MyDB'
-
-mysql = MySQL(app)
+# app.config['MYSQL_HOST'] = 'localhost'
+# app.config['MYSQL_USER'] = 'root'
+# app.config['MYSQL_PASSWORD'] = 'root'
+# app.config['MYSQL_DB'] = 'MyDB'
+#
+# mysql = MySQL(app)
 
 @app.route('/')
 def hello_world():
