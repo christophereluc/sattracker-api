d5aee5832efdd442cd2e849aa1df166d2522a69e
Christopher Luc <christopher.e.luc@gmail.com>
2019-11-09 14:56:24 -0800

    Added in rounding function and rounding of nearby's lat/lng

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask import request
 from flask_mysqldb import MySQL
+from time import time
 import requests
-import csv # for parsing beacon data
-import urllib.request # for downloading beacon csv
-from parse_csv import parse_csv # helper function to turn beacon data into sql-friendly struct
-from post_data import post_data # helper function to post beacon data using MySQL
-from json_serialize import json_serialize # helper function to return mysql as json_serialize
+import csv  # for parsing beacon data
+import urllib.request  # for downloading beacon csv
+from parse_csv import parse_csv  # helper function to turn beacon data into sql-friendly struct
+from post_data import post_data  # helper function to post beacon data using MySQL
+from json_serialize import json_serialize  # helper function to return mysql as json_serialize
 import os
 import json
 
 API_KEY = 'JWH8ZQ-G7HTPQ-KRBG9Q-47TP'
 BASE_URL = "https://www.n2yo.com/rest/v1/satellite/"
+ROUNDING_VALUE = 3
+TTL = 2 * 60  # time in seconds
 
 app = Flask(__name__)
 
@@ -23,28 +26,45 @@ app.config['MYSQL_DB'] = 'heroku_95aba217f91d579'
 
 mysql = MySQL(app)
 
-#Usage: http://127.0.0.1:5000/nearby?lat=33.865990&lng=-118.175630&&alt=0
+# Function used to round a value based on ROUNDING_VALUE
+# i.e. truncate to 3 decimal places
+def roundPosition(value):
+    return round(value, ROUNDING_VALUE)
+
+
+# Usage: http://127.0.0.1:5000/nearby?lat=33.865990&lng=-118.175630&&alt=0
 @app.route('/nearby')
 def get_nearby_satellites():
     try:
-        latitude = float(request.args.get('lat'))
-        longitude = float(request.args.get('lng'))
+        # truncate lat/long for easier caching for now
+        latitude = roundPosition(float(request.args.get('lat')))
+        longitude = roundPosition(float(request.args.get('lng')))
         altitude = float(request.args.get('alt'))
 
+        # TODO: Check cache, if present and valid, return
+
+
+        # TODO otherwise, hit API
         satellites = requests.get(
-            BASE_URL + "above/" + str(latitude) + "/" + str(longitude) + "/" + str(altitude) + "/90/18/&apiKey=" + API_KEY).json()
+            BASE_URL + "above/" + str(latitude) + "/" + str(longitude) + "/" + str(
+                altitude) + "/90/18/&apiKey=" + API_KEY).json()
         iss = requests.get(
-            BASE_URL + "above/" + str(latitude) + "/" + str(longitude) + "/" + str(altitude) + "/90/2/&apiKey=" + API_KEY).json()
+            BASE_URL + "above/" + str(latitude) + "/" + str(longitude) + "/" + str(
+                altitude) + "/90/2/&apiKey=" + API_KEY).json()
 
         satellites["above"] += iss["above"]
         data = {"data": satellites["above"]}
+
+        # TODO and cache then return
+
         return json.dumps(data)
     except Exception as e:
         print("Unexpected error:", e)
         return "{ \"error\" : \"Unexpected error.  Ensure that contains lat/lng/alt parameters\"}"
 
-#test_URLs: http://127.0.0.1:5000/tracking?id=13002&lat=33.865990&lng=-118.175630&&alt=0
-@app.route('/tracking') #This can be used for position
+
+# test_URLs: http://127.0.0.1:5000/tracking?id=13002&lat=33.865990&lng=-118.175630&&alt=0
+@app.route('/tracking')  # This can be used for position
 def get_tracking_info():
     try:
         satid = int(request.args.get('id'))
@@ -53,7 +73,8 @@ def get_tracking_info():
         altitude = float(request.args.get('alt'))
 
         satellites = requests.get(
-                BASE_URL + "radiopasses/" + str(satid) + "/" + str(latitude) + "/" + str(longitude) + "/" + str(altitude) + "/2/10/&apiKey=" + API_KEY).json()
+            BASE_URL + "radiopasses/" + str(satid) + "/" + str(latitude) + "/" + str(longitude) + "/" + str(
+                altitude) + "/2/10/&apiKey=" + API_KEY).json()
 
         data = {"data": satellites["passes"]}
 
@@ -62,15 +83,16 @@ def get_tracking_info():
         print("Unexpected error:", e)
         return "{ \"error\" : \"Unexpected error.  Ensure that contains id/lat/lng/alt parameters\"}"
 
+
 # test URL: http://127.0.0.1:5000/beacons?ids=[10032,28895]
 @app.route('/beacons')
 def print_beacon_information():
-    ids = request.args.get('ids')[1:-1].split(',') # get query string
+    ids = request.args.get('ids')[1:-1].split(',')  # get query string
     # build up SQL query
     for i, id in enumerate(ids):
         # sanitize query
         try:
-            ids[i] =  int(ids[i])
+            ids[i] = int(ids[i])
             ids[i] = 'id=' + ids[i]
         except Exception as e:
             print("invalid query string")
@@ -83,11 +105,12 @@ def print_beacon_information():
         cur.execute(dump_str)
         rows = cur.fetchall()
         result = json_serialize(rows, cur)
-        print("successfully obtained beacon data from table 'satellites'" )
+        print("successfully obtained beacon data from table 'satellites'")
         return json.dumps(result)
     except Exception as e:
         print("unable to retrieve beacon data: " + e)
         return "{ \"error\" : \"Unexpected error fetching from database\"}"
+
 
 @app.route('/update_beacons')
 def get_beacon_information():
@@ -101,7 +124,7 @@ def get_beacon_information():
         data = resp.read()
         text = data.decode('utf-8')
         open('./data/beacons.csv', 'w').write(text)
-        print( "beacon request successful")
+        print("beacon request successful")
     except urllib.error.URLError as e:
         print("Error opening beacon data url:", e.reason)
         return "{ \"error\": \"Unexpected error parsing csv data\" }"
@@ -118,13 +141,14 @@ def get_beacon_information():
     # ADD DATA TO DATABASE
     try:
         post_data(mysql, beacons)
-        data = { "data": beacons }
+        data = {"data": beacons}
         return json.dumps(data)
     except Exception as e:
         print("Error posting csv data to database:", e)
         return "{ \"error\": \"Unexpected error posting csv data\" }"
 
     return "beacon data updated"
+
 
 if __name__ == '__main__':
     app.run()
